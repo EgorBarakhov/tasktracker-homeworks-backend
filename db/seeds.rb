18b92cf5fed13f90f48b44d0f9d0a6dacfda626b
egor.barakhov <egor.barakhov@flatstack.com>
2021-06-09 23:19:55 +0300

    Create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Task.create(
+    text: Faker::Lorem.sentence(word_count: 3),
+    day: Faker::Date.forward(14),
+    reminder: Faker::Boolean.boolean
+  )
+end
